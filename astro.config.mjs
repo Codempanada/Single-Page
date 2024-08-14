17a4c0d9738e1afe1411fa412aa77c0a1634a036
Eli-ezer Reuven Ramirez Ruiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2024-08-14 00:15:33 -0400

    update url / -> /single-page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://codempanada.github.io",
-    base: "/",
+    base: "/single-page",
     publicDir: "public",
     output: "static",
     trailingSlash: 'ignore',
